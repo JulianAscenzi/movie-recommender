af2364792e22f148b7b66b7fcd26daefb1b7396e
Julián Ascenzi <julianascenzim@gmail.com>
2026-04-24 15:05:19 -0300

    Refactor test_load.py to encapsulate load_all functionality within test_load_all function

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,6 +1,13 @@
 from src.data_loader import load_all
 
-movies, ratings = load_all()
 
-print(movies.head())
-print(ratings.head())
+def test_load_all():
+    movies, ratings = load_all()
+
+    assert movies is not None
+    assert ratings is not None
+    assert len(movies) > 0
+    assert len(ratings) > 0
+
+    print(movies.head())
+    print(ratings.head())
